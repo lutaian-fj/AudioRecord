2909e5a421c754fbb32db6095e8a69ef766027c0
卢泰桉 <890425>
2017-04-25 18:29:40 +0800

    添加录音按钮和一些图片

--- AudioReccord/module_audioRecord/src/main/java/lta/com/audioRecord/ui/activity/MainActivity.java
+++ AudioReccord/module_audioRecord/src/main/java/lta/com/audioRecord/ui/activity/MainActivity.java
@@ -2,17 +2,27 @@ package lta.com.audioRecord.ui.activity;
 
 import android.os.Environment;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.File;
 
 import lta.com.audioRecord.R;
 import lta.com.audioRecord.utils.AudioRecordUtil;
 
-public class MainActivity extends BaseActivity {
-    private Button mStartBtn,mStopBtn;
-    private AudioRecordUtil mAduioRecordUtil; //工具类
+/**
+ * @author: LuTaiAn
+ * @className: MainActivity
+ * @description: 程序入口
+ * @date: 2017/4/25
+ */
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+    private AudioRecordUtil mAudioRecordUtil; //工具类
     private File mOutFile; //录音存放文件夹
+    private int startBgRes = R.drawable.start_record_audio_selector; //开始录音背景
+    private int stopBgRes = R.drawable.start_record_audio_big_selector; //停止录音背景
+    private TextView mTvPlay; //录音开启关闭按钮
+    private boolean mIsRecording = false; //是否正在录音
+
     @Override
     protected int getLayoutRes() {
         return R.layout.activity_main;
@@ -20,31 +30,36 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        mStartBtn = (Button) findViewById(R.id.btn_start);
-        mStopBtn = (Button) findViewById(R.id.btn_stop);
+        mTvPlay = (TextView) findViewById(R.id.tv_play);
+        mTvPlay.setOnClickListener(this);
+        mTvPlay.setBackgroundResource(startBgRes);
     }
 
     @Override
     protected void initData() {
-        mOutFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/AudioRecord");
-        mAduioRecordUtil = AudioRecordUtil.getInstance(mOutFile);
+        mOutFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/AudioRecord");
+        mAudioRecordUtil = AudioRecordUtil.getInstance(mOutFile);
     }
 
     @Override
     protected void initEvent() {
-        mStartBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String fileName = System.currentTimeMillis()+"record.amr";
-//                File recAudioFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/AudioRecord", fileName);
-                mAduioRecordUtil.startRecord(fileName);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.tv_play) {
+            if (mIsRecording) { //结束录音
+                mAudioRecordUtil.stopRecord();
+                mIsRecording = false;
+                mTvPlay.setBackgroundResource(startBgRes);
+                return;
             }
-        });
-        mStopBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mAduioRecordUtil.stopRecord();
-            }
-        });
+            String fileName = System.currentTimeMillis() + "record.amr";
+            mAudioRecordUtil.startRecord(fileName);
+            mTvPlay.setBackgroundResource(stopBgRes);
+            mIsRecording = true;
+        }
     }
 }
